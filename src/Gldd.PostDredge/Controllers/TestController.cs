8a326256772802a56c696e533be127bd1231ca90
Jan Paolo Go <go.janpaolo@gmail.com>
2020-05-30 14:51:45 -0500

    Update TestController.cs

--- src/Gldd.PostDredge/Controllers/TestController.cs
+++ src/Gldd.PostDredge/Controllers/TestController.cs
@@ -6,6 +6,6 @@ namespace Gldd.PostDredge.Controllers
     [Route("[controller]")]
     public class TestController : ControllerBase
     {
-        public object Get() => "test4";
+        public object Get() => "test5";
     }
 }
