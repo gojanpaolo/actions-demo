d7aabbd88748e0496572b2d10e9076074e7564b3
Jan Paolo Go <go.janpaolo@gmail.com>
2020-05-29 22:28:30 -0500

    project initial commit

---
+++ src/Gldd.PostDredge/Controllers/TestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gldd.PostDredge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        public object Get() => "test";
+    }
+}
